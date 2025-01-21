ef00f9fde5914d4025410b919c6e45de97cc7401
Oziel Sousa <ozielzin358@gmail.com>
2025-01-20 22:42:28 -0300

    Aula 5 TypeScript

---
+++ src/server.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(5, 2);
+console.log("resultado", result);
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+function sum(a: number, b: number): number {
+  return a + b
+}
+
+const result : number = sum(5,2)
+
+console.log("resultado", result);
