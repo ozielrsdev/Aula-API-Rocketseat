52dcd4d9bd9350d8df700c7a4a8011b4f35ab7ae
Oziel Sousa <ozielzin358@gmail.com>
2025-01-30 18:37:30 -0300

    Aula 9 Middleware global

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import { myMiddleware } from './middlewares/my-middleware'
 
 const PORT = 3333
 
 const app = express()
 app.use(express.json())
+
+app.use(myMiddleware)
 
 app.get('/products', (request, response) => {
   const { page, limit } = request.query
@@ -13,7 +16,7 @@ app.get('/products', (request, response) => {
 app.post('/products', (request, response) => {
   const { name, price } = request.body
   // response.send(`Produto: ${name} com o valor de ${price}`)
-  response.status(201).json({name, price})
+  response.status(201).json({ name, price })
 })
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`)
 )
