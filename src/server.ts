596c671f542b3ab4f8f5a6e3e4f9ff06e5e7501b
Oziel Sousa <ozielzin358@gmail.com>
2025-01-30 18:26:07 -0300

    Aula 8 Resposta com JSON

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,8 @@ app.get('/products', (request, response) => {
 
 app.post('/products', (request, response) => {
   const { name, price } = request.body
-  response.send(`Produto: ${name} com o valor de ${price}`)
+  // response.send(`Produto: ${name} com o valor de ${price}`)
+  response.status(201).json({name, price})
 })
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`)
 )
