21dde4173bc15b824b39b12c144b3bb3737165ba
Oziel Sousa <ozielzin358@gmail.com>
2025-01-30 18:50:02 -0300

    Aula 11 Modificando Requisição

--- src/middlewares/my-middleware.ts
+++ src/middlewares/my-middleware.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
 
 export function myMiddleware(request: Request, response: Response, next: NextFunction) {
-console.log("Passou pelo Middleware");
+  request.user_id = '123' // adicionando um id de usuário ao request
+  console.log("Passou pelo Middleware");
 
-return next()
+  return next()
 }
--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.get('/products', (request, response) => {
 app.post('/products', myMiddleware, (request, response) => {
   const { name, price } = request.body
   // response.send(`Produto: ${name} com o valor de ${price}`)
-  response.status(201).json({ name, price })
+  response.status(201).json({ name, price, user_id:request.user_id })
 })
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`)
 )
