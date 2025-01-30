f8b8e1760c66ff3f4272e401239f65d5d0227674
Oziel Sousa <ozielzin358@gmail.com>
2025-01-30 18:12:44 -0300

    Aula 7 Query Params

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ const PORT = 3333
 
 const app = express()
 
-app.get('/products:id', (request, response)=>{
- const { id } = request.params
-  response.send("Produto:" + id)
+app.get('/products', (request, response)=>{
+  const {page , limit} = request.query
+  response.send(`Pagina ${page} de ${limit}`)
 })
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}!`)
 )
