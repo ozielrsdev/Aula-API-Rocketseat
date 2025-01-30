4ebb2c8167f98979e4f98473df69e4cc6bdc8b50
Oziel Sousa <ozielzin358@gmail.com>
2025-01-30 18:22:38 -0300

    Aula 8 Corpo da Requisição

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,16 @@ import express from 'express'
 const PORT = 3333
 
 const app = express()
+app.use(express.json())
 
-app.get('/products', (request, response)=>{
-  const {page , limit} = request.query
+app.get('/products', (request, response) => {
+  const { page, limit } = request.query
   response.send(`Pagina ${page} de ${limit}`)
 })
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}!`)
+
+app.post('/products', (request, response) => {
+  const { name, price } = request.body
+  response.send(`Produto: ${name} com o valor de ${price}`)
+})
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`)
 )
