d9186bfb0cda31cc0484d44a87359c13926fa412
Oziel Sousa <ozielzin358@gmail.com>
2025-02-02 23:16:38 -0300

    Aula 15 Tratando Requisição

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { AppError } from '../utils/AppError'
 
 export class ProductsController {
   /**
@@ -18,7 +19,8 @@ export class ProductsController {
   create(request: Request, response: Response) {
     const { name, price } = request.body
     // response.send(`Produto: ${name} com o valor de ${price}`)
-    throw new Error("Erro na ROTA")
+    // throw new Error("Erro na ROTA")
+    // throw new AppError("Erro ao tentar criar um produto!", 401)
     response.status(201).json({ name, price, user_id: request.user_id })
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express'
 import { routes } from './routes'
+import { AppError } from './utils/AppError'
 
 
 const PORT = 3333
@@ -8,8 +9,11 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
-app.use((error: any, request: Request, response: Response, _:NextFunction) => {
-  response.status(500).json({message: error.message})
+app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message })
+  }
+  response.status(500).json({ message: error.message })
 })
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`)
 )
